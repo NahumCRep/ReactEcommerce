dff5e299f45e0cd775634ed349d31cc6fc3bfc5d
Gisse88 <coronel.dg@gmail.com>
2022-04-07 16:49:50 -0300

    probando logo con animacion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion"
 import { useState } from 'react'
+import Logo from '../components/Logo'
 
 const Login = () => {
   const [emailL,setEmailL] = useState("")
@@ -15,7 +16,8 @@ const Login = () => {
   return (
     <main className=' w-full h-screen pt-16 flex justify-center items-center'>
       <div className='border-2 border-white w-full sm:w-4/5 md:1/2 h-[480px] flex justify-center items-center relative'>
-        <div className="bg-palette-lightblue h-[400px] w-full rounded-md shadow-md overflow-hidden">    
+        <div className="bg-palette-lightblue h-[400px] w-full rounded-md shadow-md overflow-hidden"> 
+          <Logo size="100px"/>   
         </div>
         {showLogin?
              <motion.div className='bg-white h-full w-full sm:w-96 border-2 border-palette-teal absolute rounded-lg shadow-md py-5 px-8 flex flex-col gap-5'
@@ -37,7 +39,12 @@ const Login = () => {
                <input type="password" onChange={(e)=>{setPasswordL(e.target.value)}} 
                  className='bg-gray-100 outline-none border border-gray-100 focus:border-palette-lightblue '/>
              </div>
-             <button className='bg-palette-teal rounded-md text-white hover:opacity-80 py-3'>LogIn</button>
+             <motion.button 
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
+              className='bg-palette-teal rounded-md text-white hover:opacity-80 py-3'>
+                LogIn
+              </motion.button>
              <button>Google</button>
              <button>Facebook</button>
            </motion.div>
@@ -71,7 +78,12 @@ const Login = () => {
               <input type="password" onChange={(e)=>{setPasswordL(e.target.value)}} 
                 className='bg-gray-100 outline-none border border-gray-100 focus:border-palette-lightblue '/>
             </div>
-            <button className='bg-palette-teal rounded-md text-white hover:opacity-80 py-3'>Register</button>
+            <motion.button 
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
+              className='bg-palette-teal rounded-md text-white hover:opacity-80 py-3'>
+                Register
+            </motion.button>
           </motion.div>
         }
    
