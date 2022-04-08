a2530d0340a66e5ea408cb5598146632e5181e62
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-08 10:45:05 -0500

    brand item

---
+++ src/components/Home/BrandItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrandItem = ({brand}) => {
+  return (
+    <p className='font-neue text-xl md:text-3xl text-gray-400'>{brand}</p>
+  )
+}
+
+export default BrandItem  
