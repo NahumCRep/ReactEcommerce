3c8a6b09d20fdb655a55010e83beb2a9f4fe0bab
Gisse88 <coronel.dg@gmail.com>
2022-04-10 23:37:37 -0300

    mmm

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 import {Provider} from 'react-redux'
 import store from './store';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux'
-import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
