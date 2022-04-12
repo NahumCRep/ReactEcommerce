02f58f5d4c15748030ffbe3110d013ff1461655b
Gisse88 <coronel.dg@gmail.com>
2022-04-12 16:16:36 -0300

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
 import { FcElectronics } from 'react-icons/fc'
+import {FiMenu,FiXCircle} from 'react-icons/fi'
 import Cart from './Cart'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
     const user = useSelector(state => state.user)
-    console.log('usuario', user)
+    //console.log('usuario', user)
+    const [open, setOpen] = useState(false);
     return (
         <nav className='w-full h-16 px-12 fixed z-50 bg-white border-b-2 border-palette-lightgray flex items-center justify-between'>
             <Link to='/'>
@@ -17,8 +19,8 @@ const Navbar = () => {
                     <p className='pt-1'>ELECTRONICS</p>
                 </div>
             </Link>
-            <div className='flex gap-8'>
-                <ul className='h-full text-palette-teal text-lg font-bold flex gap-8 items-center font-josefina'>
+            <div className='flex gap-8 relative'>
+                <ul className={`${open?'inline-block':'hidden '}h-full text-white md:text-palette-teal bg-palette-teal md:bg-transparent text-lg font-bold md:flex absolute right-0 top-11 md:static gap-8 items-center font-josefina space-y-2 md:space-y-0 p-4 md:p-0 text-right`}>
                     <li>
                         <div className='flex items-center gap-2'>
                             <input type='text' className='bg-palette-lightgray w-80 rounded-lg h-8 px-3 outline-none border-none font-josefina text-sm text-center' />
@@ -42,6 +44,7 @@ const Navbar = () => {
                         </ul>
                     )
                 }
+                <button onClick={()=>{setOpen(!open)}} className='block md:hidden mr-2 md:mr-0'>{open?<FiXCircle className='h-5 w-5'/>:<FiMenu className='h-5 w-5'/>}</button>
             </div>
 
         </nav>
